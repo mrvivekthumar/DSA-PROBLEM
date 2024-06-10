a4d601151ed14eb4a0200b11467121cbd3d69e0a
mrvivekthumar <mrvivekthumar@gmail.com>
2024-06-10 12:15:57 +0530

    added folder dp problem set 1

---
+++ Dynamic Programming/DP Problem Set-1/A-Frog-1.cpp
@@ -0,0 +1,17 @@
+// link = "https://atcoder.jp/contests/dp/tasks/dp_a"
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> ar;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+    }
+
+    return 0;
+}
