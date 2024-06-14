ca34c4f79a3d5985c2fc98d8e934e46e75d3d53e
mrvivekthumar <mrvivekthumar@gmail.com>
2024-06-14 16:07:21 +0530

    completed succesfully submission

--- Dynamic Programming/DP Problem Set-1/D_Knapsack1.cpp
+++ Dynamic Programming/DP Problem Set-1/D_Knapsack1.cpp
@@ -2,23 +2,50 @@
 #include <bits/stdc++.h>
 #define ll long long
 using namespace std;
-struct activity
+
+struct item
 {
-    int wt;
-    int val;
+    ll wt;
+    ll val;
 };
 
-ll D_Knapsack(vector<activity> ar, int n, int W) {
+ll D_Knapsack(vector<item> ar, ll n, ll W)
+{
 
+    // first dimension of the dp represent the 'i'th item
+    // second dimensin of the dp represent the wt of the knapsack
+    ll dp[n + 1][W + 1];
+
+    for (ll w = 0; w <= W; w++)
+    {
+        dp[1][w] = 0;
+    }
+
+    dp[1][ar[1].wt] = ar[1].val;
+
+    for (ll i = 2; i <= n; i++)
+    {
+        for (ll w = 0; w <= W; w++)
+        {
+            dp[i][w] = dp[i - 1][w];
+            if (ar[i].wt <= w)
+            {
+                dp[i][w] = max(dp[i][w], ar[i].val + dp[i - 1][w - ar[i].wt]);
+            }
+        }
+    }
+
+    return *max_element(dp[n], dp[n] + W + 1);
 };
 
 int main()
 {
-    int n, w;
+    ll n, w;
     cin >> n >> w;
-    vector<activity> ar(n + 1);
+    vector<item> ar(n + 1);
 
-    for (int i = 0; i < n; i++)
+    // one '1' base indexing
+    for (ll i = 1; i <= n; i++)
     {
         cin >> ar[i].wt >> ar[i].val;
     }
