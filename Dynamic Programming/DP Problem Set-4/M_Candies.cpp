416ccc5e8047a1411f3f65c2886286d3dbbd0c73
mrvivekthumar <mrvivekthumar@gmail.com>
2024-07-04 17:51:06 +0530

    added dp problem set 4

---
+++ Dynamic Programming/DP Problem Set-4/M_Candies.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define ll long long
+int M_Candies(vector<int> &v, int n, int k)
+{
+    ll dp[n + 2][k + 2];
+    for (int j = 0; j <= k; j++)
+    {
+        dp[1][j] = (j > v[1]) ? 0 : 1;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j <= k; j++)
+        {
+            if (j == 0)
+            {
+                dp[i][j] = dp[i - 1][j];
+            }
+            else
+            {
+                dp[i][j] = (mod + dp[i][j - 1] + dp[i - 1][j] - ((j - v[i] - 1 >= 0) ? dp[i - 1][j - v[i] - 1] : 0)) % mod;
+            }
+        }
+    }
+
+    return dp[n][k];
+};
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> v[i];
+    }
+
+    cout << M_Candies(v, n, k);
+
+    return 0;
+}
