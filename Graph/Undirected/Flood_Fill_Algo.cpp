597c9b5b283b46df6d922844ff7f61dd829adc0b
mrvivekthumar <mrvivekthumar@gmail.com>
2024-07-18 22:12:13 +0530

    half Flood fill

---
+++ Graph/Undirected/Flood_Fill_Algo.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int R, C;
+    cin >> R >> C;
+    char input[15][50];
+
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            cin >> input[i][j];
+        }
+    }
+
+    return 0;
+}
